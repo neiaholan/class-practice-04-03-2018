be03ad60f4f5683c03650fb26ffc1cf44c0ec28f
Hunter <NOHunter.2014@MyMail.jcsu.edu>
2018-04-03 14:43:02 -0400

    first commit

---
+++ SumOfArrays.java
@@ -0,0 +1,18 @@
+
+public class SumOfArrays {
+public static void main(String [] args){
+
+int[] array = {2,3,4};
+int[] array2 = {4,5,6};
+int[] array3 = new int [3];
+
+for( int i = 0 ; i < 3 ; i++){
+int sum = array[i]+array2[i];
+array3 [i]=sum;
+
+System.out.println(array3 [i]);
+
+}
+
+}
+}
